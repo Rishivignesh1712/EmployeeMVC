132e63d80e1412088811e032c5e6b17818f09758
Administrator <Administrator@43D1A2BBDA89523>
2023-11-30 11:30:46 +0530

    Add project files.

---
+++ EmployeeMVC/Controllers/DepartmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMVC.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Postdata()
+        {
+            return View();
+        }
+    }
+}
